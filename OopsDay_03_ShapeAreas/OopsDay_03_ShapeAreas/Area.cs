149f433d406258bfcfcc820b0ea2368177833887
adhipilla-01 <167074617+adhipilla-01@users.noreply.github.com>
2024-04-15 14:49:17 +0530

    Add files via upload

---
+++ OopsDay_03_ShapeAreas/OopsDay_03_ShapeAreas/Area.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsDay_03_ShapeAreas
+{
+    internal class Area
+    {
+        private double Length;
+        private double Width;
+        private double Radius;
+
+        public Area(double Length, double Width)
+        {
+            this.Length = Length;
+            this.Width = Width;
+        }
+        public Area(double Radius)
+        {
+            this.Radius = Radius;
+        }
+        public double RectangleArea()
+        {
+            return 2 * Length * Width;
+        }
+        public double CircleArea()
+        {
+            return 3.14 * Radius * Radius;
+        }
+    }
+}
---
+++ OopsDay_03_ShapeAreas/OopsDay_03_ShapeAreas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsDay_03_ShapeAreas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Length of Rectangle : ");
+            double Length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Width of Rectangle  : ");
+            double Width = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Area of Circle      : ");
+            double Radius = Convert.ToDouble(Console.ReadLine());
+
+            Area AreaOfRectangle = new Area(Width, Length);
+            Area AreaOfCircle = new Area(Radius);
+            double RectangleArea = AreaOfRectangle.RectangleArea();
+            double CircleArea = AreaOfCircle.CircleArea();
+            Console.WriteLine("\nArea of the Shapes According to given data (parameters) : ");
+            Console.Write($"\nArea of Rectangle is {RectangleArea}");
+            Console.Write($"\nArea of Circle is {CircleArea}\n\n");
+        }
+    }
+}
