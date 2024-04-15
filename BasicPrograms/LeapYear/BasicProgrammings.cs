c5c00b4ef3d91322a0a565dee6c0502cd0bc52b3
adhipilla-01 <167074617+adhipilla-01@users.noreply.github.com>
2024-04-15 15:14:45 +0530

    Add files via upload

---
+++ BasicPrograms/LeapYear/BasicProgrammings.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class BasicProgrammings
+    {
+        public void LeapYear()
+        {
+            Console.Clear();
+            Console.Write("\nEnter year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                Console.WriteLine($"\nThe Year {year} is Leap Year having 366 days");
+            }
+            else Console.WriteLine($"\nThe Year {year} is Normal Year having 365 Days");                
+        }
+        public void Fibanocci()
+        {
+            Console.Clear();
+            Console.Write("\nEnter number of series : ");
+            int terms = Convert.ToInt32(Console.ReadLine());
+            int f1 = 0, f2 = 1, f3;
+            for (int i = 0; i < terms; i++)
+            {
+                Console.WriteLine(f1);
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+            }
+        }
+        public void MoneyDemonitation()
+        {
+            Console.Clear();
+            Console.Write("\nEnter Amount to Demonistate : ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            int[] money = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+            
+            foreach (int i in money)
+            {
+                if(amount >= i)
+                {
+                    int count = amount / i;
+                    amount = amount % i;
+                    Console.WriteLine($"\n{count} Notes/Coins of {i}");
+                }
+            }
+        }
+        public void NormalDimond()
+        {
+            Console.Clear();
+            Console.Write("\nEnter Number of Rows (Odd only): ");
+            byte rows = Convert.ToByte(Console.ReadLine());
+
+            for(int i = 1; i <= rows; i += 2)
+            {
+                int space = (rows - i) / 2;
+                for(int j = 0; j < space; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j = 0;j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            for (int i = rows - 2; i >= 1; i -= 2)
+            {
+                int space = (rows - i) / 2;
+                for(int j = 0; j < space; j++)
+                {
+                    Console.Write(" ");
+                }
+                for( int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+        public void HollowDimond()
+        {
+            Console.Clear();
+            Console.Write("\nEnter number of rows : ");
+            byte rows = Convert.ToByte(Console.ReadLine());
+
+            for (int i = 1; i <= rows; i += 2)
+            {
+                int space = (rows - i) / 2;
+                for (int j = 0; j <= space; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    if (j == 0 || j == i - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            //printing lower diamond
+            for(int i = rows - 2; i >= 1; i -= 2)
+            {
+                int space = (rows - i) / 2 + 1;
+                for( int j = 0; j < space;j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j = 0; j < i; j++)
+                {
+                    if (j == 0 || j == i - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ BasicPrograms/LeapYear/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            a:
+            Console.Clear();
+            Console.Write("Enter a option :\n1.Leap Year\n2.Fibanocci Series\n3.Money Demonitation\n4.Normal Dimond\n5.Hollow Dimond : ");
+            byte option = Convert.ToByte(Console.ReadLine());
+
+            BasicProgrammings BasicPrograms = new BasicProgrammings();
+            if (option == 1) 
+            { 
+                BasicPrograms.LeapYear();
+                Console.Write("\nDo you want to Back to Home [ Y / N] : ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                if (ch == 'Y' || ch == 'y')
+                {
+                    goto a;
+                }
+            }
+            else if (option == 2) 
+            { 
+                BasicPrograms.Fibanocci();
+                Console.Write("\nDo you want to Back to Home [ Y / N] : ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                if (ch == 'Y' || ch == 'y')
+                {
+                    goto a;
+                }
+            }
+            else if (option == 3) 
+            {
+                BasicPrograms.MoneyDemonitation();
+                Console.Write("\nDo you want to Back to Home [ Y / N] : ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                if (ch == 'Y' || ch == 'y')
+                {
+                    goto a;
+                }
+            }
+            else if (option == 4) 
+            {
+                BasicPrograms.NormalDimond();
+                Console.Write("\nDo you want to Back to Home [ Y / N] : ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                if (ch == 'Y' || ch == 'y')
+                {
+                    goto a;
+                }
+            }
+            else if (option == 5) 
+            {
+                BasicPrograms.HollowDimond();
+                Console.Write("\nDo you want to Back to Home [ Y / N] : ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                if (ch == 'Y' || ch == 'y')
+                {
+                    goto a;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid option !");
+                return;
+            }
+         }
+    }
+}
